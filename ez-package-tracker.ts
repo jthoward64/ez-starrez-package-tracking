2f2fc40c512b75344b120c0f1409bb2bdc27b646
Joshua Tag Howard <tag@jthoward.dev>
2023-08-15 17:36:09 -0400

    Add download/update URLs

--- ez-package-tracker.ts
+++ ez-package-tracker.ts
@@ -5,6 +5,8 @@
 // @description  Adds an easy-to-use form to the Quick Information page of StarRez
 // @author       Joshua Tag Howard
 // @match        https://starport.uky.edu
+// @updateURL    https://jthoward64.github.io/ez-starrez-package-tracking/ez-package-tracker.js
+// @downloadURL  https://jthoward64.github.io/ez-starrez-package-tracking/ez-package-tracker.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uky.edu
 // @grant        none
 // ==/UserScript==
