bc7a6484ea92daf0f27aa7fd83107339cf233cab
Joshua Tag Howard <tag@jthoward.dev>
2023-08-15 17:39:41 -0400

    Fix match URL

--- ez-package-tracker.ts
+++ ez-package-tracker.ts
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         StarRez EZ Package Tracking
 // @namespace    http://tampermonkey.net/
-// @version      1.1.2
+// @version      1.1.3
 // @description  Adds an easy-to-use form to the Quick Information page of StarRez
 // @author       Joshua Tag Howard
-// @match        https://starport.uky.edu
+// @match        https://starport.uky.edu/*
 // @updateURL    https://jthoward64.github.io/ez-starrez-package-tracking/ez-package-tracker.js
 // @downloadURL  https://jthoward64.github.io/ez-starrez-package-tracking/ez-package-tracker.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=uky.edu
